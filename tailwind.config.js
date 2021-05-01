a2201f6e536b0b7c36e090edda71728adc7ad4e1
Branislav Puzder <bbrang2@gmail.com>
2021-05-01 10:03:46 +0200

    upload

---
+++ tailwind.config.js
@@ -0,0 +1,44 @@
+module.exports = {
+  purge: {
+    enabled: true,
+    content: ["./src/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "kp-purple": {
+          DEFAULT: "#5C1D6B",
+        },
+        "kp-yellow": {
+          DEFAULT: "#FDD602",
+        },
+        "kp-orange": {
+          DEFAULT: "#FF6F39",
+        },
+        "kp-pink": {
+          DEFAULT: "#FF3179",
+        },
+        "kp-purple-light": {
+          DEFAULT: "#B41EB2",
+        },
+        "kp-blue": {
+          DEFAULT: "#38A6F3",
+        },
+        "kp-green": {
+          DEFAULT: "#23D10C",
+        },
+        "kp-red": {
+          DEFAULT: "#D51636",
+        },
+      },
+    },
+    fontFamily: {
+      sans: ["Poppins", "sans-serif"],
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
