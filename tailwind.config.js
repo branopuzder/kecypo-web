a5d99bae4c18fdd1c47b6a70bc3c396c42c2270f
Branislav Puzder <bbrang2@gmail.com>
2022-04-03 17:46:22 +0200

    Formatted changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,44 +1,44 @@
 module.exports = {
-  purge: {
-    enabled: true,
-    content: ["./*.html"],
-  },
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        "kp-purple": {
-          DEFAULT: "#5C1D6B",
-        },
-        "kp-yellow": {
-          DEFAULT: "#FDD602",
-        },
-        "kp-orange": {
-          DEFAULT: "#FF6F39",
-        },
-        "kp-pink": {
-          DEFAULT: "#FF3179",
-        },
-        "kp-purple-light": {
-          DEFAULT: "#B41EB2",
-        },
-        "kp-blue": {
-          DEFAULT: "#38A6F3",
-        },
-        "kp-green": {
-          DEFAULT: "#23D10C",
-        },
-        "kp-red": {
-          DEFAULT: "#D51636",
-        },
-      },
+    purge: {
+        enabled: true,
+        content: ["./*.html"],
     },
-    fontFamily: {
-      sans: ["Poppins", "sans-serif"],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                "kp-purple": {
+                    DEFAULT: "#5C1D6B",
+                },
+                "kp-yellow": {
+                    DEFAULT: "#FDD602",
+                },
+                "kp-orange": {
+                    DEFAULT: "#FF6F39",
+                },
+                "kp-pink": {
+                    DEFAULT: "#FF3179",
+                },
+                "kp-purple-light": {
+                    DEFAULT: "#B41EB2",
+                },
+                "kp-blue": {
+                    DEFAULT: "#38A6F3",
+                },
+                "kp-green": {
+                    DEFAULT: "#23D10C",
+                },
+                "kp-red": {
+                    DEFAULT: "#D51636",
+                },
+            },
+        },
+        fontFamily: {
+            sans: ["Poppins", "sans-serif"],
+        },
     },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 };
