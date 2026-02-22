d27a8161d558b0c36bd8367968d5f64b3e95f080
Branislav Puzder <34285009+branopuzder@users.noreply.github.com>
2026-02-22 16:26:05 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,29 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [tailwindcss()],
+  root: './',
+  publicDir: 'src/static',
   server: {
     port: 3000,
-    open: true
+    open: true,
+    host: true
   },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     minify: 'terser',
-    sourcemap: false
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name].[hash][extname]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
+      }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': '/src'
+    }
   }
 });
